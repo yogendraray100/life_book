292d9268c8a6a96c2573ec896d2babbb56a95ba3
yogendraray100 <yogendraray100@gmail.com>
2024-05-09 13:19:43 +0545

    update15 commit

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ const multer  = require('multer')
 const { v4:uuidv4 } = require('uuid') ;
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../uploads')
+    cb(null, './uploads')
   },
   filename: function (req, file, cb) {
    const random = uuidv4()
